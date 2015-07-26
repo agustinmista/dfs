6fa36a9729dcd1bca095bd4ba5931ce7659b912e
Paula <paulajessicab@gmail.com>
2015-07-26 03:17:58 -0300

    changes in worker receiver

--- posix/Worker.c
+++ posix/Worker.c
@@ -1,27 +1,66 @@
 #include "Common.h"
 #include "Worker.h"
 
-/*
-struct mq_attr attr;
+/*struct mq_attr attr;
 attr.mq_maxmsg = 300;
-attr.mq_msgsize = MSG_SIZE;
-attr.mq_flags = 0;		//VER
-*/
+attr.mq_msgsize = 10000;
+attr.mq_flags = 0;*/
 
-void *worker(void *id){
+
+void *worker(void *w_info){
 	
-	int wid = *((int *) id);
+	Worker_Info *info = (Worker_Info *)w_info;
+    int wid = info->id;
+    mqd_t wqueue = info->queue;
+    File *wfiles = info->files;
     
-    File *files = NULL;
+    //File *files = 
+    //files->content = NULL;
+    //files->next = NULL;
+    char message[100];
+   char *parsing, *saveptr;
 	
+			
     while(1){
+        
+        memset(message, 0, 100);
+              
+        if(mq_receive(wqueue, message, 100, NULL) >= 0){
+			
+			printf("... \n");
+			
+			parsing = strtok_r(message, " ", &saveptr);
+			if (strcmp (parsing,"LSD") == 0)
+			{
+				parsing = strtok_r(NULL, " ", &saveptr);
+				if (parsing == NULL){
+					printf("LSD\n");
+				}else
+					printf("error de comando \n");
+			}
+			else if (strcmp (parsing,"DEL") == 0)
+				printf ("DEL\n");
+			else if (strcmp (parsing,"CRE") == 0)
+				printf ("CRE\n");
+			else if (strcmp (parsing,"OPN") == 0)
+				printf ("OPN\n");
+			else if (strcmp (parsing,"WRT") == 0)
+				printf ("WRT\n");
+			else if (strcmp (parsing,"REA") == 0)
+				printf ("REA\n");
+			else if (strcmp (parsing,"CLO") == 0)
+				printf ("CLO\n");
+			else if (strcmp (parsing,"BYE") == 0)
+				printf ("BYE\n");
+			else
+				printf("error de comando");
         //---
         //--- Where the magic happens!
         //---
-    }    
+		}
+	}
     
-    
-    mq_close(worker_queues[wid]);
+    mq_close(wqueue);
     return 0;
 
 }
@@ -37,8 +76,15 @@ int init_workers(){
         if((worker_queues[i] = mq_open(worker_name, O_RDWR | O_CREAT, 0666, NULL)) == (mqd_t) -1)
             ERROR("\nDFS_SERVER: Error opening message queue for workers\n");
         
+        //worker_files[i] = malloc(sizeof(File)*n_Files_Worker);
+        
+        Worker_Info *newWorker;
+        newWorker->id = i;
+        newWorker->queue = worker_queues[i];
+        newWorker -> files = NULL;
+        
         // Spawn a new worker
-        pthread_create(&workers[i], NULL, worker, &i);
+        pthread_create(&workers[i], NULL, worker, &newWorker);
         
     }
     return 0;
